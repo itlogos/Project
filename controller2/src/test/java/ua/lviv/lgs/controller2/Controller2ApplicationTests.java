91bc14965d78303a544613b7428229a57ba570dc
olegnik <olegnik11@ukr.net>
2023-10-28 20:18:10 +0300

    Add domain

---
+++ controller2/src/test/java/ua/lviv/lgs/controller2/Controller2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.lgs.controller2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Controller2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
